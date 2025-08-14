dbfbaec504ee338d6d1e9efc534ad0e1ac7a1a1e
Akash <akashpoddar492@gmail.com>
2025-08-14 12:00:36 +0530

    Add Book class with title , author and publication year

--- src/Book.java
+++ src/Book.java
@@ -5,7 +5,12 @@ public class Book {
         String author = "William Shakespeare";
         int publicationYear = 1890;
 
-        System.out.println("The great book " + title + " written by "+ author+" in "+ publicationYear);
+        String title2 = "Motion";
+        String author2 = "Newton";
+        int publicationYear2 = 1660;
+
+        System.out.println("The great book " + title + " written by "+ author+" in "+ publicationYear2);
+        System.out.println("The great book " + title2 + " written by "+ author2+" in "+ publicationYear2);
 
     }
 }
