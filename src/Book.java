c17deb58c729185ddc89eb327bcd95f4ff9368df
Akash <akashpoddar492@gmail.com>
2025-08-14 11:49:19 +0530

    Initial commit: Set up BookInfoProject with Book class

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+
+    public static void main(String[] args) {
+        String title = "Romeo Juliet";
+        String author = "William Shakespeare";
+        int publicationYear = 1890;
+
+        System.out.println("The great book " + title + " written by "+ author+" in "+ publicationYear);
+
+    }
+}
